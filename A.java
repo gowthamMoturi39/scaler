d92c3b381e4027a93febba9be739d39bee25929b
Default User <defaultuser@Defaults-MacBook-Air.local>
2024-05-04 18:30:12 +0530

    Merge conflict resolved

--- A.java
+++ A.java
@@ -1,13 +1,10 @@
 public class A {
-<<<<<<< HEAD
     private int e;
     private int f;
     private int g;
     private int h;
-=======
     private int a;
     private int b;
     private int c;
     private int d;
->>>>>>> feat1
 }
